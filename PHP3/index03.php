f87624d63f39bf16f8229892ba962b5491027511
mindaugaskadzys <mindaugas.kadzys@gmail.com>
2020-05-15 19:01:46 +0100

    pridetas php3 namu darbas

---
+++ PHP3/index03.php
@@ -0,0 +1,34 @@
+<?php
+    $x = 10;
+    $y = 7;
+
+    $z = $x + $y;
+    echo $x. ' + '. $y. ' = '. $z;
+    echo '<br>';
+
+    $z = $x - $y;
+    echo $x. ' - '. $y. ' = '. $z;
+    echo '<br>';
+
+    $z = $x * $y;
+    echo $x. ' * '. $y. ' = '. $z;
+    echo '<br>';
+
+    $z = $x / $y;
+    echo $x. ' / '. $y. ' = '. $z;
+    echo '<br>';
+
+    $z = $x % $y;
+    echo $x. ' % '. $y. ' = '. $z;
+    echo '<br>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP3 namu darbai</title>
+    </head>
+    <body>
+    </body>
+</html>
