2d7ca0df24f97184ecb5bd1020880fc88c1b1e89
mindaugaskadzys <mindaugas.kadzys@gmail.com>
2020-05-15 15:49:08 +0100

    pridetas PHP2 namu darbas

---
+++ PHP2/index02.php
@@ -0,0 +1,19 @@
+<?php
+    $country = 'Lietuva';
+    $city = 'Vilnius';
+    $population = 574221;
+
+    echo $city. ' - Lietuvos sostinė';
+    echo '<br>';
+    echo "$city ir $country sparčiai auga";
+?>
+<!DOCTYPE html>
+    <html lang="lt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP2</title>
+    </head>
+    <body>
+    </body>
+</html>
