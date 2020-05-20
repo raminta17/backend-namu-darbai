be9520be8fb18cc7aeb2e7c9fa2990d119318c51
mindaugaskadzys <mindaugas.kadzys@gmail.com>
2020-05-20 14:05:36 +0100

    pridetas PHP10 namu darbas

---
+++ PHP10/index10.php
@@ -0,0 +1,17 @@
+<?php
+    function multiply($a,$b){
+        $area = $a * $b;
+        echo "Stačiakampio plotas yra $area kv.vnt.";
+    }
+    multiply(3,4);
+?>
+<!DOCTYPE html>
+<html lang="lt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP10 namu darbas</title>
+    </head>
+    <body>
+    </body>
+</html>
